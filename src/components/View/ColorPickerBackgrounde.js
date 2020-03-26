d1e213b6bb7724efa6a69c88ce98facb14068ac0
Georss <b.george7.gb@gmail.com>
2020-03-26 10:50:10 +0200

    Update ColorPickerBackgrounde.js

--- src/components/View/ColorPickerBackgrounde.js
+++ src/components/View/ColorPickerBackgrounde.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './ColorPicker.css';
 
-const ColorPickerBackgrounde = ({ color, index, getColorBackgrounde }) => (
-    <div
-        className="box-color-picker"
-        style={{ backgroundColor: color }}
-        onClick={getColorBackgrounde}
-        id={index}
-    />
-);
+const ColorPickerBackgrounde = ({ color, index, getColorBackgrounde, ID }) => {
+
+    const activeBorder = ID == index ? 'border-active' : '';
+
+    return (
+        <div
+            className={`box-color-picker ${activeBorder}`}
+            style={{ backgroundColor: color }}
+            onClick={getColorBackgrounde}
+            id={index}
+        />
+    );
+};
 
 export default ColorPickerBackgrounde;
