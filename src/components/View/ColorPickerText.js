f811d704d2eb3ca54ee5c4c919565a2c0c7ef5c8
Georss <b.george7.gb@gmail.com>
2020-03-26 10:51:33 +0200

    Update ColorPickerText.js

--- src/components/View/ColorPickerText.js
+++ src/components/View/ColorPickerText.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './ColorPicker.css';
 
-const ColorPickerText = ({ color, index, getColorText }) => (
-    <div
-        className="box-color-picker"
-        style={{ backgroundColor: color }}
-        onClick={getColorText}
-        id={index}
-    />
-);
+const ColorPickerText = ({ color, index, getColorText, ID }) => {
+
+    const activeBorder = ID == index ? 'border-active' : '';
+
+    return (
+        <div
+            className={`box-color-picker ${activeBorder}`}
+            style={{ backgroundColor: color }}
+            onClick={getColorText}
+            id={index}
+        />
+    );
+};
 
 export default ColorPickerText;
